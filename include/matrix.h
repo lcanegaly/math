5976e9d5788f5651c23ceacccd34c88066f15ccd
lee <lcanegaly@gmail.com>
2023-07-01 20:53:23 -0400

    Added a Vec4 and Mat4x4 type.

--- include/matrix.h
+++ include/matrix.h
@@ -31,6 +31,39 @@ struct Type_Mat2x2 {
   ColumnType data[2];
 };
 
+template<typename T>
+struct Type_Mat4x4 {
+  typedef Type_Vec4<T> ColumnType;
+  typedef Type_Vec4<T> RowType;
+  using Col = ColumnType;
+  
+  Type_Mat4x4() {
+    data[0] = Col(1, 0, 0, 0);
+    data[1] = Col(0, 1, 0, 0);
+    data[2] = Col(0, 0, 1, 0);
+    data[3] = Col(0, 0, 0, 1);
+  }
+  Type_Mat4x4(Col x, Col y, Col z, Col w) {
+    data[0] = x; 
+    data[1] = y; 
+    data[2] = z; 
+    data[3] = w; 
+  }
+  Type_Mat4x4 operator=(const Type_Mat4x4<T>& other) {
+    data[0] = other[0];
+    data[1] = other[1];
+    data[2] = other[0];
+    data[3] = other[0];
+    return *this;
+  }
+  ColumnType& operator[](std::size_t i){
+    return data[i];
+  }
+ private:
+  // Column major ordering
+  ColumnType data[4];
+};
+
 typedef Type_Mat2x2<float> Matrix2x2;
 
 template<typename T>
--- include/vector.h
+++ include/vector.h
@@ -24,6 +24,36 @@ struct Type_Vec2 {
   T y;
 };
 
+// A 4 component vector type
+template<typename T>
+struct Type_Vec4 {
+  Type_Vec4(T x, T y, T z, T w): x{x}, y{y}, z{z}, w{w}{}
+  Type_Vec4(): x{0}, y{0}, z{0}, w{0}{}
+
+  Type_Vec4 operator-(const Type_Vec4& other){
+    return Type_Vec4{x - other.x, y - other.y, z - other.z, w - other.w};
+  }  
+  Type_Vec4 operator=(const Type_Vec4& other) {
+    x = other.x;
+    y = other.y;
+    z = other.z;
+    w = other.w;
+    return Type_Vec4(x,y,z,w); 
+  }
+  Type_Vec4 operator+(const Type_Vec4& other) {
+    x += other.x;
+    y += other.y;
+    z += other.z;
+    w += other.w;
+    return Type_Vec4(x,y,z,w);
+  }
+  T x;
+  T y;
+  T z;
+  T w;
+};
+
+
 template<typename T>
 std::ostream& operator<<(std::ostream& os, const Type_Vec2<T>& v){
     os << v.x << "," << v.y << "\n";
@@ -36,5 +66,8 @@ typedef Type_Vec2<FloatType> Vec2;
 
 typedef int IntType;
 typedef Type_Vec2<IntType> IVec2;
+
+typedef Type_Vec4<FloatType> Vec4;
+typedef Type_Vec4<IntType> IVec4;
 
 #endif
